1f5250c1cd358d25162c8837ee666bd2a1e779c2
BlueSkyClouds <mayobluesky@gmail.com>
2023-12-25 06:04:03 -0400

    update

--- tampermonkey/mteam/Auto Refresh on Error Page.js
+++ tampermonkey/mteam/Auto Refresh on Error Page.js
@@ -1,6 +1,7 @@
 // ==UserScript==
-// @name     Auto Refresh on Error Page
+// @name     自动刷新error
 // @match    https://kp.m-team.cc/details.php?id=*
+// @match    https://ticket.m-team.io/
 // @icon         https://kp.m-team.cc/favicon.ico
 // @grant    none
 // ==/UserScript==
